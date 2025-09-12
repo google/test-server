eab1f407c648ebf3ff68e5d01c4d6d64199bbd97
wanlin31 <wanlindu@google.com>
2025-09-12 14:59:05 +0000

    allow test-name to include parent path.

--- sdks/python/sample/test_sample.py
+++ sdks/python/sample/test_sample.py
@@ -11,9 +11,13 @@ class TestSampleWithServer:
     def test_should_receive_200_from_proxied_github(self):
         """Tests that a request to the proxy returns a successful response."""
         print("[PyTest] Making request to test-server proxy for www.github.com...")
+
+        custom_headers = {
+            'Test-Name': 'models/python-sample-test',
+        }   
         
         # Use the 'requests' library for a simpler HTTP call
-        response = requests.get("http://localhost:17080/", timeout=10)
+        response = requests.get("http://localhost:17080/", headers=custom_headers, timeout=10)
 
         # Pytest uses simple 'assert' statements for checks
         assert response.status_code == 200
